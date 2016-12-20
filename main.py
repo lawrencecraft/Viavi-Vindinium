9d8b03866a4d5607bbf8a3ac964c40739e405ca2
Lawrence Craft <lawrence.craft@gmail.com>
2016-12-20 07:56:56 +0000

    Updating with extra changes

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import vindinium.bots.states.state_definitions as states
 def main():
     # Create a vindinium client
     client = vindinium.Client(
-        server='http://aigamesvm:9000',
-        key='sui35va8',
-        # key='vibet5m0',
+        # server='http://aigamesvm:9000',
+        # key='sui35va8',
+        key='vibet5m0',
         mode='arena', #or training
         n_turns=300,
         open_browser=True
--- vindinium/bots/states/state_definitions.py
+++ vindinium/bots/states/state_definitions.py
@@ -20,13 +20,25 @@ class GoToMineState(BaseState):
         self.state_name = "GoToMineState"
         self.transitions = [
             Transition(lambda t:
-                       self.hero_health_is_less_than(20)(t) and
+                       self.hero_health_is_less_than(30)(t) and
                        self.gold_is_greater_than(2)(t), lambda: GoToTavernState()),
             Transition(lambda t: self.hero_health_is_less_than(70)(t) and
                                  self.nearest_tavern_is()(t) == 1 and
                                  self.gold_is_greater_than(2)(t),
+                       lambda: GoToTavernState()),
+            Transition(lambda t: self.nearest_tavern_is()(t) > t.hero.life + 20,
                        lambda: GoToTavernState())
         ]
 
     def execute_move(self, bot_state):
         return self.go_to_nearest_mine(bot_state)
+
+class CombatState(BaseState):
+    def __init__(self):
+        super(CombatState, self).__init__()
+        self.state_name = "CombatState"
+        self.transitions = []
+
+    def execute_move(self, bot_state):
+        pass
+
